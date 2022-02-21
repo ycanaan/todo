caaf1b6e2e4bc704a8fc728f0aad6ce437abb0f6
sujeathpareddy <80287737+sujeathpareddy@users.noreply.github.com>
2022-02-21 00:18:04 -0500

    Added task completion

--- main.js
+++ main.js
@@ -16,3 +16,13 @@ function deleteListItem(item) {
     console.log(item);
     item.parentNode.removeChild(item);
 }
+
+function completeListItem(item) {
+    if (item.checked) { // true if the input checkbox is checked
+        item.parentNode.style.textDecoration = "line-through";
+        // in css, this would be: "text-decoration: line-through"
+    } else {
+        item.parentNode.style.textDecoration = "none";
+        // in css, this would be: "text-decoration: none"
+    }
+}
